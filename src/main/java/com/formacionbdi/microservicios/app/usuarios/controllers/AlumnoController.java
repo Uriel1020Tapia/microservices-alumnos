e48be4e24344f8bbcd48b9a5cf92103515287d5c
Uriel Tapia Avalos <uriel.tapia@inai.org.mx>
2022-01-03 21:02:13 -0600

    Conectando servicio usuarios como cliente de eureka

--- src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/AlumnoController.java
+++ src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/AlumnoController.java
@@ -5,12 +5,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.microservicios.app.usuarios.models.entity.Alumno;
@@ -21,17 +19,19 @@ import com.formacionbdi.microservicios.app.usuarios.services.IAlumnoService;
 */
 
 @RestController
+@RequestMapping("api/v1/")
 public class AlumnoController {
 
 	@Autowired
 	private IAlumnoService alumnoService;
 	
-	@GetMapping("/")
+	 // List All Alumnos
+    @RequestMapping(value = "alumnos", method = RequestMethod.GET)
 	public ResponseEntity<?> listar(){
 		return  ResponseEntity.ok().body(alumnoService.findAll());
 	}
-	
-	@GetMapping("/{id}")
+    // List One Alumno
+    @RequestMapping(value = "alumnos/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> ver(@PathVariable Long id){
 		
 		Optional<Alumno> o = alumnoService.findById(id);
@@ -42,13 +42,15 @@ public class AlumnoController {
 		return  ResponseEntity.ok(o.get());
 	}
 	
-	@PostMapping("/save")
+ // Create New Alumno
+    @RequestMapping(value = "alumnos", method = RequestMethod.POST)
 	public ResponseEntity<?> crear(@RequestBody Alumno alumno){
 		Alumno alumnoResp = alumnoService.save(alumno);
 		return ResponseEntity.status(HttpStatus.CREATED).body(alumnoResp);
 	}
 	
-	@PutMapping("/{id}")
+    // Update Alumno with PUT
+    @RequestMapping(value = "alumnos/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<?> editar(@RequestBody Alumno alumnoReq,@PathVariable Long id){
 	Optional<Alumno> o = alumnoService.findById(id);
 		
@@ -64,8 +66,8 @@ public class AlumnoController {
 		
 	}
 	
-	
-	@DeleteMapping("/{id{")
+ // Delete Alumno
+    @RequestMapping(value = "alumnos/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<?> eliminar(@PathVariable Long id){
 		alumnoService.deleteById(id);
 		return ResponseEntity.noContent().build();
